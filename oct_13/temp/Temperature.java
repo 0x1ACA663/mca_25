ee848f0aa53bc1d1f0c2a7e807d00811a337d190
Priyanuj Gogoi <0x1aca663@gmail.com>
2025-10-13 10:22:08 +0530

    d

--- oct_13/temp/Temperature.java
+++ oct_13/temp/Temperature.java
@@ -1,9 +1,9 @@
 public class Temperature {
-  public static double toFahrenheit(double celsius) {
+  public double toFahrenheit(double celsius) {
     return (9 / 5 * celsius) + 32; 
   }
 
-  public static double toCelsius(double farenheit) {
+  public double toCelsius(double farenheit) {
     return (farenheit - 32) * 5 / 9;
   }
 }
