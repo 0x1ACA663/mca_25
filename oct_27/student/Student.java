bcb946beb5675b13b3a240e28229e1bd4b0bced3
Priyanuj Gogoi <0x1aca663@gmail.com>
2025-10-30 08:44:17 +0530

    d

--- oct_27/student/Student.java
+++ oct_27/student/Student.java
@@ -2,14 +2,29 @@ public class Student extends Grade {
   String name;
   int rollNo;
   
-  Student(String name, int rollNo, int marks) {
+  public Student(String name, int rollNo, int marks) {
     this.name = name;
     this.rollNo = rollNo;
     this.marks = marks;
   }
 
+  public String display() {
+    return String.format(
+      "Student Name: %s%nStudent Roll No: %d%nStudent Marks: %d (Grade: %s)%n",
+      name,
+      rollNo,
+      marks,
+      calculateGrade()
+    );
+  }
+
   @Override
   public String toString() {
-    return "Student(name=" + name + ", rollNo=" + rollNo + ", marks=" + marks + ")";
+    return String.format(
+      "Student(name=%s, rollNo=%i, marks=%i)",
+      name,
+      rollNo,
+      marks
+    );
   }
 }
--- oct_27/student/StudentGrade.java
+++ oct_27/student/StudentGrade.java
@@ -1,12 +1,7 @@
 void main() {
   Student stud1 = new Student("John", 1, 58);
-  Student stud2 = new Student("John", 2, 88);
+  Student stud2 = new Student("Harish", 2, 88);
 
-  System.out.printf(
-    "Grade of %s's having marks %i = %i%n",
-    stud1.name,
-    stud1.marks,
-    stud1.calculateGrade()
-  );
-  System.out.println(stud2);
+  System.out.println(stud1.display());
+  System.out.print(stud2.display());
 }
